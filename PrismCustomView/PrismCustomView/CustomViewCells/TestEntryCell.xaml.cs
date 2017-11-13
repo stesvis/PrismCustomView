6010ebb2bc41e00bb8ab8b7b476e5f01cc13c4f6
Cristian Merli <stesvis@gmail.com>
2017-11-13 10:59:31 -0700

    First commit

---
+++ PrismCustomView/PrismCustomView/CustomViewCells/TestEntryCell.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrismCustomView.CustomViewCells
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestEntryCell : ViewCell
+    {
+        public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(TestEntryCell));
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TestEntryCell));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public TestEntryCell()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
--- PrismCustomView/PrismCustomView/ViewModels/MainPageViewModel.cs
+++ PrismCustomView/PrismCustomView/ViewModels/MainPageViewModel.cs
@@ -10,10 +10,27 @@ namespace PrismCustomView.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService) 
-            : base (navigationService)
+        private string _myLabelProperty;
+        public string MyLabelProperty
+        {
+            get { return _myLabelProperty; }
+            set { SetProperty(ref _myLabelProperty, value); }
+        }
+
+        private string _myTextProperty;
+        public string MyTextProperty
+        {
+            get { return _myTextProperty; }
+            set { SetProperty(ref _myTextProperty, value); }
+        }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
         {
             Title = "Main Page";
+
+            MyLabelProperty = "Label 1";
+            MyTextProperty = "Hello world 1";
         }
     }
 }
