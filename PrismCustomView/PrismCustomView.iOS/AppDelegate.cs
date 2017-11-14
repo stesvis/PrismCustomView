f167506767c87d8bdc90d9bf45809e123f73331c
Cristian Merli <stesvis@gmail.com>
2017-11-14 08:59:58 -0700

    Ran CodeMaid

--- PrismCustomView/PrismCustomView.iOS/AppDelegate.cs
+++ PrismCustomView/PrismCustomView.iOS/AppDelegate.cs
@@ -1,23 +1,20 @@
-﻿using Foundation;
-using UIKit;
-using DryIoc;
+﻿using DryIoc;
+using Foundation;
 using Prism.DryIoc;
+using UIKit;
 
 namespace PrismCustomView.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to
     // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
+        // This method is invoked when the application has loaded and is ready to run. In this method
+        // you should instantiate the window, load the UI into it and then make the window visible.
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
@@ -31,7 +28,6 @@ namespace PrismCustomView.iOS
     {
         public void RegisterTypes(IContainer container)
         {
-            
         }
     }
 }
--- PrismCustomView/PrismCustomView/CustomViewCells/TestEntryCell.xaml.cs
+++ PrismCustomView/PrismCustomView/CustomViewCells/TestEntryCell.xaml.cs
@@ -1,49 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace PrismCustomView.CustomViewCells
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TestEntryCell : ViewCell
-	{
-		public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(TestEntryCell));
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestEntryCell : ViewCell
+    {
+        public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(TestEntryCell));
 
-		public string Label
-		{
-			get { return (string)GetValue(LabelProperty); }
-			set { SetValue(LabelProperty, value); }
-		}
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
 
-		public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TestEntryCell), defaultBindingMode: BindingMode.TwoWay);
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TestEntryCell), defaultBindingMode: BindingMode.TwoWay);
 
-		public string Text
-		{
-			get { return (string)GetValue(TextProperty); }
-			set { SetValue(TextProperty, value); }
-		}
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
 
-		public TestEntryCell()
-		{
-			InitializeComponent();
-			/*So, after a lot of testing, I found out that if you set the BindingContext individually
+        public TestEntryCell()
+        {
+            InitializeComponent();
+            /*So, after a lot of testing, I found out that if you set the BindingContext individually
 			 on each element like this, it works... The explanation for this is simple. A ViewCell
 			 is not like other containers (i.e StackLayout, Grid, etc) where setting the BindingContext
 			 to it will also be the same for the children, so, in this case (like happens with the
 			 ViewCells in ListViews) the elements inside have a different Binding ContextMy recommendation
 			 could be: As normally in these custom controls you have everything in a StackLayout or a Grid,
 			 you can set the BindingContext to 'this' on the Layout and let the children be binded
-			 to the same context. Like this (you can uncomment the following line and comment 
+			 to the same context. Like this (you can uncomment the following line and comment
 			 the other two, the result is the same:*/
 
-			//myStack.BindingContext = this;
-			myLabel.BindingContext = this;
-			myText.BindingContext = this;
-		}
-	}
+            //myStack.BindingContext = this;
+            myLabel.BindingContext = this;
+            myText.BindingContext = this;
+        }
+    }
 }
