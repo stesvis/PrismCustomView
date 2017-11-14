a6edaa9c2eeb5a23e9e686127ba55f437fc6733d
Luis Pulido <Luis.Pulido@cineplex.com>
2017-11-14 10:34:47 -0500

    Set the BindingContext correctly. Explanation in the file as a comment

--- PrismCustomView/PrismCustomView/CustomViewCells/TestEntryCell.xaml.cs
+++ PrismCustomView/PrismCustomView/CustomViewCells/TestEntryCell.xaml.cs
@@ -9,36 +9,41 @@ using Xamarin.Forms.Xaml;
 
 namespace PrismCustomView.CustomViewCells
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TestEntryCell : ViewCell
-    {
-        public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(TestEntryCell), propertyChanged: OnLabelChanged);
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TestEntryCell : ViewCell
+	{
+		public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(TestEntryCell));
 
-        private static void OnLabelChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            var myview = bindable as TestEntryCell;
-            myview.myLabel.Text = newValue as string;
-        }
+		public string Label
+		{
+			get { return (string)GetValue(LabelProperty); }
+			set { SetValue(LabelProperty, value); }
+		}
 
-        public string Label
-        {
-            get { return (string)GetValue(LabelProperty); }
-            set { SetValue(LabelProperty, value); }
-        }
+		public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TestEntryCell), defaultBindingMode: BindingMode.TwoWay);
 
-        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TestEntryCell));
+		public string Text
+		{
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
 
-        public string Text
-        {
-            get { return (string)GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
-        }
+		public TestEntryCell()
+		{
+			InitializeComponent();
+			/*So, after a lot of testing, I found out that if you set the BindingContext individually
+			 on each element like this, it works... The explanation for this is simple. A ViewCell
+			 is not like other containers (i.e StackLayout, Grid, etc) where setting the BindingContext
+			 to it will also be the same for the children, so, in this case (like happens with the
+			 ViewCells in ListViews) the elements inside have a different Binding ContextMy recommendation
+			 could be: As normally in these custom controls you have everything in a StackLayout or a Grid,
+			 you can set the BindingContext to 'this' on the Layout and let the children be binded
+			 to the same context. Like this (you can uncomment the following line and comment 
+			 the other two, the result is the same:*/
 
-        public TestEntryCell()
-        {
-            InitializeComponent();
-
-            BindingContext = this;
-        }
-    }
+			//myStack.BindingContext = this;
+			myLabel.BindingContext = this;
+			myText.BindingContext = this;
+		}
+	}
 }
