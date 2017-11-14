d20407b6d8c393ba695e2c1fbc03b8ea40d5750c
Cristian Merli <stesvis@gmail.com>
2017-11-14 09:17:12 -0700

    Reformatted with arguments info

--- PrismCustomView/PrismCustomView/CustomViewCells/TestEntryCell.xaml.cs
+++ PrismCustomView/PrismCustomView/CustomViewCells/TestEntryCell.xaml.cs
@@ -6,7 +6,10 @@ namespace PrismCustomView.CustomViewCells
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TestEntryCell : ViewCell
     {
-        public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(TestEntryCell));
+        public static readonly BindableProperty LabelProperty = BindableProperty.Create(
+            propertyName: nameof(Label),
+            returnType: typeof(string),
+            declaringType: typeof(TestEntryCell));
 
         public string Label
         {
@@ -14,7 +17,11 @@ namespace PrismCustomView.CustomViewCells
             set { SetValue(LabelProperty, value); }
         }
 
-        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(TestEntryCell), defaultBindingMode: BindingMode.TwoWay);
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            propertyName: nameof(Text),
+            returnType: typeof(string),
+            declaringType: typeof(TestEntryCell),
+            defaultBindingMode: BindingMode.TwoWay);
 
         public string Text
         {
@@ -26,14 +33,14 @@ namespace PrismCustomView.CustomViewCells
         {
             InitializeComponent();
             /*So, after a lot of testing, I found out that if you set the BindingContext individually
-			 on each element like this, it works... The explanation for this is simple. A ViewCell
-			 is not like other containers (i.e StackLayout, Grid, etc) where setting the BindingContext
-			 to it will also be the same for the children, so, in this case (like happens with the
-			 ViewCells in ListViews) the elements inside have a different Binding ContextMy recommendation
-			 could be: As normally in these custom controls you have everything in a StackLayout or a Grid,
-			 you can set the BindingContext to 'this' on the Layout and let the children be binded
-			 to the same context. Like this (you can uncomment the following line and comment
-			 the other two, the result is the same:*/
+             on each element like this, it works... The explanation for this is simple. A ViewCell
+             is not like other containers (i.e StackLayout, Grid, etc) where setting the BindingContext
+             to it will also be the same for the children, so, in this case (like happens with the
+             ViewCells in ListViews) the elements inside have a different Binding ContextMy recommendation
+             could be: As normally in these custom controls you have everything in a StackLayout or a Grid,
+             you can set the BindingContext to 'this' on the Layout and let the children be binded
+             to the same context. Like this (you can uncomment the following line and comment
+             the other two, the result is the same:*/
 
             //myStack.BindingContext = this;
             myLabel.BindingContext = this;
